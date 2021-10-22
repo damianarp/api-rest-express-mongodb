fece0dd30ddf641b3388f5503d07d291ecb67ef6
damianarp <damianarp@gmail.com>
2021-10-22 01:10:35 -0300

    Protegiendo las rutas de la Api Rest en el servidor con el token.

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,7 @@
 // Importaciones necesarias
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuario_model');
 const Joi = require('joi');
@@ -24,9 +26,27 @@ const schema = Joi.object({
 });
 
 
+////////// MIDDLEWARE DE VERIFICACIÓN DE TOKEN //////////
+
+// Creamos un middleware para verificar el token.
+let verificarToken = (req, res, next) => {
+    // Obtenemos el token, según Postman.
+    let token = req.get('Authorization');
+    // Verificamos el token con jwt y le pasamos el SEED del módulo config, y una función callback que nos dirá si hay un error o simplemento pasa los datos decodificados.
+    jwt.verify(token, config.get('configToken.SEED'), (err, decoded) => {
+        // Si existe un error. Error 401 (Not authorized).
+        if(err) return res.status(401).json(err);
+        // Si no hay error, obtenemos el usuario que está viniendo en el payload a través del decoded.
+        req.usuario = decoded.usuario;
+        // Llamamos a la función next() del middleware para que se continúe con la ejecución de la app y esta no se cuelgue.
+        next();
+    });
+}
+
+
 ////////// PETICIÓN GET //////////
 
-ruta.get('/', (req, res) => {
+ruta.get('/', verificarToken, (req, res) => {
     // Resultado. Será una promesa porque utiliza la función asíncrona listarUsuariosActivos().
     let resultado = listarUsuariosActivos();
     // Manejamos la promesa.
